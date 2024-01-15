e0087e419f88ec0d4595226562f5b76bd29b384a
tilerahul <tilerahul8068@gmail.com>
2024-01-15 19:09:50 +0530

    css prefix

---
+++ script.js
@@ -0,0 +1,27 @@
+let endDate = new Date(2024, 0, 15, 23, 36);
+
+const dateId = document.getElementById("date");
+dateId.innerHTML = endDate.toDateString();
+
+let innerDiv = document.getElementsByClassName("inner-div");
+console.log(innerDiv)
+let input = document.querySelectorAll("input");
+
+
+
+
+setInterval(function clock (){    
+    let now = new Date();
+    let diff = endDate - now;
+
+    if(diff<0){
+        innerDiv[0].innerHTML = "Expired !!!!";
+
+        return;
+    }
+    input[0].value = Math.floor(diff/(1000 * 60 * 60 * 24));
+    input[1].value = Math.floor((diff%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+    input[2].value = Math.floor((diff%(1000 * 60 * 60))/(1000 * 60));
+    input[3].value = Math.floor((diff%(1000 * 60))/(1000));
+}, 1000)
+
